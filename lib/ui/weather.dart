73700db3910d72c14a9fa534c79d2f1e57af18a2
EYoung75 <eyoung75.ey@gmail.com>
2019-03-05 16:29:21 -0700

    Added initial background image and laid out simple font and menu button

---
+++ lib/ui/weather.dart
@@ -0,0 +1,47 @@
+import "package:flutter/material.dart";
+
+class Weather extends StatefulWidget {
+  @override
+  _WeatherState createState() => _WeatherState();
+}
+
+class _WeatherState extends State<Weather> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Weather"),
+        backgroundColor: Colors.red,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Image.asset(
+              "images/first-weather.jpg",
+              height: 1200.0,
+              width: 500.0,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(0.0, 25.0, 25.0, 0.0),
+            alignment: Alignment.topRight,
+            child: Text(
+              "Denver",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontStyle: FontStyle.italic),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
